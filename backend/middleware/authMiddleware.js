97c0adb83eb6a1e3d0fdc21db013ba832d8ff746
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-09 03:02:20 -0700

    Backend Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,66 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized. Invalid token.');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+})
+
+const protectR = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            let userId = req.params.id;
+            let user = await User.findById(userId);
+
+            const mySecret = `${user.password}-${user.createdAt}`;
+
+            const decoded = jwt.verify(token, mySecret);
+
+            req.user = await User.findById(decoded.id);
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized. Invalid token.');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+})
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
+}
+
+export { protect, protectR, admin }
