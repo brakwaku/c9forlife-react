b13404730d05e96f8b9df4f768aebc2713b01853
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-10 07:02:00 -0700

    Scroll to top of page on route change

--- frontend/src/screens/InternalErrorScreen.js
+++ frontend/src/screens/InternalErrorScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useScrollToTop } from '../utilities/scrollToTop';
 
 const InternalErrorScreen = () => {
     document.title = "C9ForLife | Internal Error";
+    useScrollToTop();
     return (
         <div className='container'>
             <h1>Sorry, an error occured and we are working to get it resolved. Please come back later</h1>
