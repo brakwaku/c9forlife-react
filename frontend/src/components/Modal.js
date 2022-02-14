1f374212031b22af72f002a0bb2d0b6aa795d37b
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-14 16:20:42 -0700

    Hook activity modal to portal div in index html

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,8 +1,9 @@
-import React, { useRef, useState, useEffect, useCallback } from "react";
-import styled from "styled-components";
-import media from "../utilities/media";
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+import media from '../utilities/media';
 
-const Modal = ({
+const Backdrop = ({
   showModal,
   setShowModal,
   theActivity,
@@ -23,7 +24,7 @@ const Modal = ({
 
   const keyPress = useCallback(
     (e) => {
-      if (e.key === "Escape" && showModal) {
+      if (e.key === 'Escape' && showModal) {
         setShowModal(false);
       }
     },
@@ -46,80 +47,100 @@ const Modal = ({
   };
 
   useEffect(() => {
-    document.addEventListener("keydown", keyPress);
-    return () => document.removeEventListener("keydown", keyPress);
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
   }, [keyPress]);
-
   return (
-    <>
-      {showModal ? (
-        <Background
-          className="animate__animated animate__fadeIn"
-          onClick={closeModal}
-          ref={modalRef}
-        >
-          <ModalWrapper
-            className="animate__animated animate__fadeInDown"
-            showModal={showModal}
-          >
-            <div>
-              <div className="modal-header">
-                <h5 className="modal-title">Edit Activity</h5>
+    <Background
+      className="animate__animated animate__fadeIn"
+      onClick={closeModal}
+      ref={modalRef}
+    >
+      <ModalWrapper
+        className="animate__animated animate__fadeInDown"
+        showModal={showModal}
+      >
+        <div>
+          <div className="modal-header">
+            <h5 className="modal-title">Edit Activity</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={() => setShowModal((prev) => !prev)}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
+          <div className="modal-body">
+            <form onSubmit={editHandler}>
+              <div className="form-group">
+                <label htmlFor="title">Name of activity</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Activity name"
+                  required
+                  value={title}
+                  onChange={handleActivityTitle}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Description"
+                  required
+                  value={description}
+                  onChange={handleActivityDescription}
+                ></textarea>
+              </div>
+              <input type="text" hidden defaultValue={id} />
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-success">
+                  Update
+                </button>
                 <button
                   type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
+                  className="btn btn-secondary"
+                  aria-label="Close modal"
                   onClick={() => setShowModal((prev) => !prev)}
                 >
-                  <span aria-hidden="true">&times;</span>
+                  Close
                 </button>
               </div>
+            </form>
+          </div>
+        </div>
+      </ModalWrapper>
+    </Background>
+  );
+};
 
-              <div className="modal-body">
-                <form onSubmit={editHandler}>
-                  <div className="form-group">
-                    <label htmlFor="title">Name of activity</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Activity name"
-                      required
-                      value={title}
-                      onChange={handleActivityTitle}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                      type="text"
-                      className="form-control"
-                      placeholder="Description"
-                      required
-                      value={description}
-                      onChange={handleActivityDescription}
-                    ></textarea>
-                  </div>
-                  <input type="text" hidden defaultValue={id} />
-                  <div className="modal-footer">
-                    <button type="submit" className="btn btn-success">
-                      Update
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      aria-label="Close modal"
-                      onClick={() => setShowModal((prev) => !prev)}
-                    >
-                      Close
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </ModalWrapper>
-        </Background>
-      ) : null}
+const Modal = ({
+  showModal,
+  setShowModal,
+  theActivity,
+  updateActivity,
+  dispatch,
+}) => {
+  return (
+    <>
+      {showModal
+        ? createPortal(
+            <Backdrop
+              showModal={showModal}
+              setShowModal={setShowModal}
+              theActivity={theActivity}
+              updateActivity={updateActivity}
+              dispatch={dispatch}
+            />,
+            document.getElementById('modal-root')
+          )
+        : null}
     </>
   );
 };
@@ -127,18 +148,13 @@ const Modal = ({
 export default Modal;
 
 const Background = styled.div`
-  /* width: 100vw; */
-  height: 100vh;
-  /* top: -6.7vh;
-  left: -6.3vw; */
-  top: 0vh;
-  left: 0vw;
-  right: 0vw;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
   background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  justify-content: center;
-  align-items: center;
-  z-index: 99999;
+  position: absolute;
+  z-index: 9999;
   padding: 2rem;
 `;
 
