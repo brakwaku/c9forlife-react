aa5c1c5f1b3d2dcba8b8ff5f33cf2b0be34e0499
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-17 23:07:49 -0700

    Bug fix

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import InputField from '../components/InputField';
