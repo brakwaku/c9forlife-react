60abc3104ba4fa433363931ea08990b21aa7a5d1
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-19 13:34:34 -0700

    Removed redundant code

--- frontend/src/screens/ArchiveScreen.js
+++ frontend/src/screens/ArchiveScreen.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import { getUserDetails } from "../actions/userActions";
-import heroImage from "../assets/arch.jpg";
-import Hero from "../components/Hero";
-import styled from "styled-components";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { getUserDetails } from '../actions/userActions';
+import heroImage from '../assets/arch.jpg';
+import Hero from '../components/Hero';
+import styled from 'styled-components';
 import { useScrollToTop } from '../utilities/scrollToTop';
 
 const ArchiveScreen = () => {
-  document.title = "C9ForLife | Archive";
+  document.title = 'C9ForLife | Archive';
   useScrollToTop();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,41 +21,50 @@ const ArchiveScreen = () => {
   const userDetails = useSelector((state) => state.userDetails);
   const {
     loading: loadingGetUserDetails,
-    success: successGetUserDetails,
     error: errorGetUserDetails,
     user,
   } = userDetails;
 
-  const [theUser, setTheUser] = useState(userInfo);
-
   useEffect(() => {
     if (userInfo === null || !userInfo) {
-      navigate("/login");
+      navigate('/login');
     } else {
-      if (!user) {
-        dispatch(getUserDetails(userInfo._id));
-      } else {
-        setTheUser(user);
-      }
+      dispatch(getUserDetails(userInfo._id));
     }
-  }, [dispatch, userInfo, navigate, successGetUserDetails, user]);
+  }, [dispatch, userInfo, navigate]);
 
   return (
     <MainWrapper>
       <div>
         <Hero image={heroImage} head="ARCHIVES" primary="white" />
       </div>
+      <Link to="/dashboard" className="btn btn-light my-3">
+        <i className="far fa-arrow-alt-circle-left"></i> Go Back
+      </Link>
       <div className="pt-3">
         {loadingGetUserDetails && <Loader />}
         {errorGetUserDetails && (
           <Message variant="danger">{errorGetUserDetails}</Message>
         )}
-        {theUser.archive.length > 0 ? theUser.archive.map((activity) => (
+        {user && user.archive.length > 0 ? (
+          user.archive.map((activity) => (
             <div className="container" key={activity._id}>
-            <h5 className="font-weight-bold">{activity.archId.title}</h5>
-            <h6><b>Description:</b><br /> {activity.archId.description}</h6><hr />
+              <h5 className="font-weight-bold">{activity.archId.title} {activity.quantity > 1 ? `(${activity.quantity} times)` : ''}</h5>
+              <h6>
+                <b>Description:</b>
+                <br /> {activity.archId.description}
+              </h6>
+              <hr />
             </div>
-        )) : <center><h5>No items in here yet. They will show once they are archived by you.</h5></center>}
+          ))
+        ) : (
+          <center>
+            <h5>
+              No items in here yet. They will show once they are archived by
+              you.
+            </h5>
+          </center>
+        )}
       </div>
     </MainWrapper>
   );
